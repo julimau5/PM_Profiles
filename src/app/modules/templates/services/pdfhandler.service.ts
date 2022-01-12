17884b058f013f2aa28684f38930d726946d9442
Julian Castro <jmcastrol@unal.edu.co>
2022-01-12 21:48:11 +0100

    pdf handler service crated and injected in test page

--- src/app/modules/templates/services/pdfhandler.service.ts
+++ src/app/modules/templates/services/pdfhandler.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PDFhandlerService {
+  constructor() {}
 
-  constructor() { }
+  public openPDF(data: any): void {
+    // let DATA = document.getElementById('htmlData');
+
+    html2canvas(data).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+
+      PDF.save('angular-demo.pdf');
+    });
+  }
 }
--- src/app/modules/templates/templates-dashboard.component.ts
+++ src/app/modules/templates/templates-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PDFhandlerService } from './services/pdfhandler.service';
 
 @Component({
   selector: 'app-templates-dashboard',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplatesDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private pdfHandler: PDFhandlerService
+  ) { }
 
   ngOnInit(): void {
   }
